8f99cd6f1bade1db583d6dd3aa1964fe123d9f6c
rbdgemzo <rbdgemzo@gmail.com>
2026-02-11 11:35:21 +0800

    simplify HpPrinter.java

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer {
-
-
     @Override
     public void print(String message) {
         System.out.println("HP 印表機 : " + message);
