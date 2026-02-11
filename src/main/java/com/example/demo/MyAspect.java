eed7fd396819d1acaf1ca3830fb02b6cad2dacd1
rbdgemzo <rbdgemzo@gmail.com>
2026-02-11 12:52:33 +0800

    refactor MyAspect.java by using @around

--- src/main/java/com/example/demo/MyAspect.java
+++ src/main/java/com/example/demo/MyAspect.java
@@ -1,26 +1,25 @@
 package com.example.demo;
 
-import org.aspectj.lang.annotation.After;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Aspect
 @Component
 public class MyAspect {
-    private Date start;
-    private Date end;
+    @Around("execution(* com.example.demo..*.print(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        Date start = new Date();
 
-    @Before("execution(* com.example.demo..*.print(..))")
-    public void before() {
-        start = new Date();
-    }
+        Object obj = pjp.proceed();
 
-    @After("execution(* com.example.demo..*.print(..))")
-    public void after() {
-        end = new Date();
+        Date end = new Date();
         long time = end.getTime() - start.getTime();
         System.out.println("總共執行了 : " + time + " ms");
+
+        return obj;
     }
 }
