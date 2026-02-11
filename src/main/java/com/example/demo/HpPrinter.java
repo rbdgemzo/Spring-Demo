597ff57d12e874d0cd7768674e2a16085b9abdac
rbdgemzo <rbdgemzo@gmail.com>
2026-02-11 11:32:46 +0800

    Revert HpPrinter.java

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class HpPrinter implements Printer {
+    @Value("${print.name:Unknown}")
+    private String name;
+    @Value("${printer.count}")
+    private int count;
+
     @Override
     public void print(String message) {
-        System.out.println("HP 印表機 : " + message);
+        System.out.println(name +  " 印表機 : " + message);
+        System.out.println("剩餘使用次數 : " + --count);
     }
 }
