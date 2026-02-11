604224b3b93a9bd3cd671e2a51534e8c1d5c5de3
rbdgemzo <rbdgemzo@gmail.com>
2026-02-11 14:45:17 +0800

    add @RequestMapping("/detail") on public class MyController

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/detail")
 @RestController
 public class MyController {
     @RequestMapping("/product")
