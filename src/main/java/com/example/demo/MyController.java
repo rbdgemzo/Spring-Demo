20ecf510f07dafef829ca1f836c934642310b35e
rbdgemzo <rbdgemzo@gmail.com>
2026-02-13 11:46:54 +0800

    demo request method

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,27 +1,33 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyController {
-    @RequestMapping("/product")
-    public Store product() {
-        Store store = new Store();
-        List<String> list = new ArrayList<>();
-        list.add("蘋果");
-        list.add("橘子");
-        store.setProductList(list);
-        return store;
+    @RequestMapping("/test1")
+    public String test1(@RequestParam Integer id,
+                        @RequestParam(defaultValue = "Amy", name = "testName") String name) {
+        System.out.println("id: " + id);
+        System.out.println("name: " + name);
+        return "Hello test1";
     }
 
-    @RequestMapping("/user")
-    public Student user() {
-        Student student = new Student();
-        student.setName("Judy");
-        return student;
+    @RequestMapping("/test2")
+    public String test2(@RequestBody Student student) {
+        return "Hello test2";
+    }
+
+    @RequestMapping("/test3")
+    public String test3(@RequestHeader String info) {
+        System.out.println("info 的值為: " + info);
+        return "Hello test3";
+    }
+
+    @RequestMapping("/test4/{id}/{name}")
+    public String test4(@PathVariable Integer id,
+                        @PathVariable String name) {
+        System.out.println("path id: " + id);
+        System.out.println("path name: " + name);
+        return "Hello test4";
     }
 }
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
 public class Student {
-    private String name;
+    Integer id;
+    String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
